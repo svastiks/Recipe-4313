0637152debe3180ece8c99b7205acb6c0d4af6dc
Bartosz Gwozdz <bartoszgwozdz@gmail.com>
2021-01-16 22:10:31 +0100

    Initial commit, index.html

---
+++ src/main/java/dev/gwozdz/DemoRecipe/DemoRecipeApplication.java
@@ -0,0 +1,13 @@
+package dev.gwozdz.DemoRecipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoRecipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoRecipeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/gwozdz/DemoRecipe/DemoRecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.gwozdz.DemoRecipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoRecipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
