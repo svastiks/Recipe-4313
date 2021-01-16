461be3cc86acd3d1b71f402e0a5f1e889bbf439b
Bartosz Gwozdz <bartoszgwozdz@gmail.com>
2021-01-16 22:55:41 +0100

    Add Model: Recipe, Ingredient, Notes, UnitOfMeasure

---
+++ src/main/java/dev/gwozdz/DemoRecipe/model/Ingredient.java
@@ -0,0 +1,55 @@
+package dev.gwozdz.DemoRecipe.model;
+
+import javax.persistence.*;
+
+@Entity
+public class Ingredient {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+    private Double quantity;
+
+    @OneToOne(fetch = FetchType.EAGER)
+    private UnitOfMeasure uom;
+
+    @ManyToOne
+    private Recipe recipe;
+
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Double quantity) {
+        this.quantity = quantity;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+}
