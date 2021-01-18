8706da7d78be1ae4c95e361bcd2b2e0f506efecf
Bartosz Gwozdz <bartoszgwozdz@gmail.com>
2021-01-18 12:48:58 +0100

    Add Difficulty, Category with @ManyToMany, set datasource.url

---
+++ src/main/java/dev/gwozdz/DemoRecipe/model/Difficulty.java
@@ -0,0 +1,5 @@
+package dev.gwozdz.DemoRecipe.model;
+
+public enum Difficulty {
+    EASY, MID, HARD
+}
--- src/main/java/dev/gwozdz/DemoRecipe/model/Recipe.java
+++ src/main/java/dev/gwozdz/DemoRecipe/model/Recipe.java
@@ -22,11 +22,20 @@ public class Recipe {
     @Lob
     private Byte[] image;
 
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
+
+
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
     private Set<Ingredient> ingredients;
+
+    @ManyToMany
+    @JoinTable(name="recipe_category",
+    joinColumns = @JoinColumn(name="recipe_id"), inverseJoinColumns = @JoinColumn(name="category_id"))
+    private Set<Category> categories;
 
     public Long getId() {
         return id;
@@ -114,5 +123,12 @@ public class Recipe {
 
     public void setIngredients(Set<Ingredient> ingredients) {
         this.ingredients = ingredients;
+    }
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
     }
 }
