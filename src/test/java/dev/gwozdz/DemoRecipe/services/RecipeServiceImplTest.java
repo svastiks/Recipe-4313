70873cb7a5d349a0e56234dc4a15af6f430696be
Bartosz Gwozdz <bartoszgwozdz@gmail.com>
2021-01-27 21:09:01 +0100

    Add test getAllRecipes returning empty set when not initialized

--- src/test/java/dev/gwozdz/DemoRecipe/services/RecipeServiceImplTest.java
+++ src/test/java/dev/gwozdz/DemoRecipe/services/RecipeServiceImplTest.java
@@ -1,5 +1,6 @@
 package dev.gwozdz.DemoRecipe.services;
 
+import dev.gwozdz.DemoRecipe.model.Recipe;
 import dev.gwozdz.DemoRecipe.repositories.RecipeRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -7,6 +8,11 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Set;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.emptyCollectionOf;
+import static org.hamcrest.Matchers.hasSize;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.BDDMockito.then;
 
@@ -25,5 +31,15 @@ class RecipeServiceImplTest {
         recipeService.getAllRecipes();
         //then
         then(recipeRepository).should().findAll();
+    }
+
+    @Test
+    void getAllRecipesShouldReturnEmptySet(){
+        //given
+        //when
+        Set<Recipe> retrievedRecipes = recipeService.getAllRecipes();
+        //then
+        assertThat(retrievedRecipes, emptyCollectionOf(Recipe.class));
+
     }
 }
