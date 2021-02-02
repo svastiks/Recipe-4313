53e7af61756fbbb42f66b380cd3524a085cb499c
Bartosz Gwozdz <bartoszgwozdz@gmail.com>
2021-02-02 21:45:12 +0100

    Add RecipeShowController.java

--- src/main/java/dev/gwozdz/DemoRecipe/controllers/RecipeShowController.java
+++ src/main/java/dev/gwozdz/DemoRecipe/controllers/RecipeShowController.java
@@ -1,2 +1,24 @@
-package dev.gwozdz.DemoRecipe.controllers;public class RecipeShowController {
+package dev.gwozdz.DemoRecipe.controllers;
+
+import dev.gwozdz.DemoRecipe.services.RecipeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class RecipeShowController {
+
+    private RecipeService recipeService;
+
+    public RecipeShowController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
+
+    @RequestMapping("/recipe/show/{id}")
+    public String showById(@PathVariable String id, Model model){
+        model.addAttribute("recipe", recipeService.getRecipeById(Long.parseLong(id)));
+
+        return "recipe/show";
+    }
 }
