aac82e9560e84ceda5e1f4523e7e39e8e61c5319
Bartosz Gwozdz <bartoszgwozdz@gmail.com>
2021-02-09 19:22:18 +0100

    Add newRecipe() mapping to recipeform.html, and Add saveOrUpdate() PostMapping for new recipe

--- src/main/java/dev/gwozdz/DemoRecipe/controllers/RecipeController.java
+++ src/main/java/dev/gwozdz/DemoRecipe/controllers/RecipeController.java
@@ -1,9 +1,12 @@
 package dev.gwozdz.DemoRecipe.controllers;
 
+import dev.gwozdz.DemoRecipe.commands.RecipeCommand;
 import dev.gwozdz.DemoRecipe.services.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -19,5 +22,18 @@ public class RecipeController {
     public String showById(@PathVariable String id, Model model){
         model.addAttribute("recipe", recipeService.getRecipeById(Long.parseLong(id)));
         return "recipe/show";
+    }
+
+    @RequestMapping("/recipe/new")
+    public String newRecipe(Model model){
+        model.addAttribute("recipe", new RecipeCommand());
+        return "recipe/recipeform";
+    }
+
+    @PostMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command){
+        RecipeCommand recipeCommandSaved = recipeService.saveRecipeCommand(command);
+
+        return "redirect:/recipe/show/" + recipeCommandSaved.getId();
     }
 }
