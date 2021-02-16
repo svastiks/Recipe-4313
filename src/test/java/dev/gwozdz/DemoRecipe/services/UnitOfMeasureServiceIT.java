ac53d77e331ba0c8317f636e06dad746ed86d646
Bartosz Gwozdz <bartoszgwozdz@gmail.com>
2021-02-16 13:55:38 +0100

    Add Integration tests for new functionalities

--- src/test/java/dev/gwozdz/DemoRecipe/services/UnitOfMeasureServiceIT.java
+++ src/test/java/dev/gwozdz/DemoRecipe/services/UnitOfMeasureServiceIT.java
@@ -1,13 +1,19 @@
 package dev.gwozdz.DemoRecipe.services;
 
+import dev.gwozdz.DemoRecipe.commands.UnitOfMeasureCommand;
 import dev.gwozdz.DemoRecipe.converters.UnitOfMeasureCommandToUnitOfMeasure;
 import dev.gwozdz.DemoRecipe.converters.UnitOfMeasureToUnitOfMeasureCommand;
 import dev.gwozdz.DemoRecipe.repositories.UnitOfMeasureRepository;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Set;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -22,4 +28,12 @@ class UnitOfMeasureServiceIT {
     @Autowired
     UnitOfMeasureRepository unitOfMeasureRepository;
 
+    @Test
+    void getAllUnitOfMeasureCommandsShouldReturnSetOfUoms(){
+        //given
+        //when
+        Set<UnitOfMeasureCommand> unitOfMeasureCommandSet = unitOfMeasureService.getAllUnitsOfMeasureCommands();
+        //then
+        assertThat(unitOfMeasureCommandSet, Matchers.<UnitOfMeasureCommand>iterableWithSize(greaterThan(0)));
+    }
 }
