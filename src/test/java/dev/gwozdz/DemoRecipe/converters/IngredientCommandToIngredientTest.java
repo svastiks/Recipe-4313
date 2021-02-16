2ccde0d9bdf1283804bd99ba58737b66e36826a3
Bartosz Gwozdz <bartoszgwozdz@gmail.com>
2021-02-16 11:46:02 +0100

    Add test for new field recipeId() + Refactor

--- src/test/java/dev/gwozdz/DemoRecipe/converters/IngredientCommandToIngredientTest.java
+++ src/test/java/dev/gwozdz/DemoRecipe/converters/IngredientCommandToIngredientTest.java
@@ -1,8 +1,10 @@
 package dev.gwozdz.DemoRecipe.converters;
 
 import dev.gwozdz.DemoRecipe.commands.IngredientCommand;
+import dev.gwozdz.DemoRecipe.commands.RecipeCommand;
 import dev.gwozdz.DemoRecipe.commands.UnitOfMeasureCommand;
 import dev.gwozdz.DemoRecipe.model.Ingredient;
+import dev.gwozdz.DemoRecipe.model.Recipe;
 import dev.gwozdz.DemoRecipe.model.UnitOfMeasure;
 import dev.gwozdz.DemoRecipe.repositories.RecipeRepository;
 import org.junit.jupiter.api.BeforeEach;
@@ -13,17 +15,24 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
 
+import java.util.Optional;
+
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.atMost;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 @MockitoSettings(strictness = Strictness.STRICT_STUBS)
 @ExtendWith(MockitoExtension.class)
 class IngredientCommandToIngredientTest {
+
+    private final static Long ID_GIVEN = Long.valueOf(1l);
+    private final static String DESCRIPTION_GIVEN = "description";
+    private final static Double QUANTITY_GIVEN = Double.valueOf(1d);
+    private final static UnitOfMeasureCommand UNIT_OF_MEASURE_COMMAND_GIVEN = new UnitOfMeasureCommand();
+    private final static UnitOfMeasure UOM_COMMAND_GIVEN = new UnitOfMeasure();
+    private final static Long RECIPE_ID = Long.valueOf(2l);
 
     private IngredientCommandToIngredient converter;
 
@@ -61,29 +70,53 @@ class IngredientCommandToIngredientTest {
     void convertShouldReturnProperValues() {
         //given
         IngredientCommand ingredientCommandGiven = prepareIngredientCommand();
-
-        UnitOfMeasureCommand uomGiven = new UnitOfMeasureCommand();
-        ingredientCommandGiven.setUom(uomGiven);
-        UnitOfMeasure uomCommandGiven = new UnitOfMeasure();
-        given(uomConverter.convert(uomGiven)).willReturn(uomCommandGiven);
+        given(uomConverter.convert(UNIT_OF_MEASURE_COMMAND_GIVEN)).willReturn(UOM_COMMAND_GIVEN);
         //when
         Ingredient ingredientConverted = converter.convert(ingredientCommandGiven);
         //then
-        assertThat(ingredientConverted.getId(), equalTo(ingredientCommandGiven.getId()));
-        assertThat(ingredientConverted.getDescription(), equalTo(ingredientCommandGiven.getDescription()));
-        assertThat(ingredientConverted.getUom(), equalTo(uomCommandGiven));
-        assertThat(ingredientConverted.getQuantity(), equalTo(ingredientCommandGiven.getQuantity()));
-        verify(uomConverter, atMost(1)).convert(uomGiven);
+        assertThat(ingredientConverted.getId(), equalTo(ID_GIVEN));
+        assertThat(ingredientConverted.getDescription(), equalTo(DESCRIPTION_GIVEN));
+        assertThat(ingredientConverted.getUom(), equalTo(UOM_COMMAND_GIVEN));
+        assertThat(ingredientConverted.getQuantity(), equalTo(QUANTITY_GIVEN));
+        verify(uomConverter, atMost(1)).convert(UNIT_OF_MEASURE_COMMAND_GIVEN);
+    }
+
+    @Test
+    void convertShouldHandleNullRecipeIdValue(){
+        //given
+        IngredientCommand ingredientCommandGiven = prepareIngredientCommand();
+        given(uomConverter.convert(UNIT_OF_MEASURE_COMMAND_GIVEN)).willReturn(UOM_COMMAND_GIVEN);
+        //when
+        Ingredient ingredientConverted = converter.convert(ingredientCommandGiven);
+        //then
+        verify(uomConverter, atMost(1)).convert(UNIT_OF_MEASURE_COMMAND_GIVEN);
+        assertThat(ingredientConverted.getRecipe(), nullValue());
+    }
+
+    @Test
+    void convertShouldHandleNotNullRecipeIdValue(){
+        //given
+        IngredientCommand ingredientCommandGiven = prepareIngredientCommand();
+        ingredientCommandGiven.setRecipeId(RECIPE_ID);
+        Recipe recipeGiven = new Recipe();
+        recipeGiven.setId(RECIPE_ID);
+        Optional<Recipe> optionalRecipeGiven = Optional.of(recipeGiven);
+        given(recipeRepository.findById(RECIPE_ID)).willReturn(optionalRecipeGiven);
+        given(uomConverter.convert(UNIT_OF_MEASURE_COMMAND_GIVEN)).willReturn(UOM_COMMAND_GIVEN);
+        //when
+        Ingredient ingredientConverted = converter.convert(ingredientCommandGiven);
+        //then
+        verify(uomConverter, only()).convert(UNIT_OF_MEASURE_COMMAND_GIVEN);
+        verify(recipeRepository, only()).findById(RECIPE_ID);
+        assertThat(ingredientConverted.getRecipe(), is(recipeGiven));
     }
 
     private IngredientCommand prepareIngredientCommand() {
         IngredientCommand ingredientCommandGiven = new IngredientCommand();
-        Long idGiven = Long.valueOf(1l);
-        String descriptionGiven = "description";
-        Double quantityGiven = Double.valueOf(1d);
-        ingredientCommandGiven.setId(idGiven);
-        ingredientCommandGiven.setDescription(descriptionGiven);
-        ingredientCommandGiven.setQuantity(quantityGiven);
+        ingredientCommandGiven.setId(ID_GIVEN);
+        ingredientCommandGiven.setDescription(DESCRIPTION_GIVEN);
+        ingredientCommandGiven.setQuantity(QUANTITY_GIVEN);
+        ingredientCommandGiven.setUom(UNIT_OF_MEASURE_COMMAND_GIVEN);
         return ingredientCommandGiven;
     }
 }
