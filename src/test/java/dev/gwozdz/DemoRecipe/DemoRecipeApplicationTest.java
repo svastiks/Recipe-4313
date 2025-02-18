109c054fc8b974c2535e222bfe3798940ddbf132
svastik <svastiksharma13@gmail.com>
2025-02-18 18:56:35 -0500

    added test case for /DemoRecipeApplication.java

---
+++ src/test/java/dev/gwozdz/DemoRecipe/DemoRecipeApplicationTest.java
@@ -0,0 +1,13 @@
+package dev.gwozdz.DemoRecipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoRecipeApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // This test ensures the Spring Boot application starts up correctly
+    }
+}
