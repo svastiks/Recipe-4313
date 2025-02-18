9cb6a3e759cc7f9d533abb133715fe8495dfdbfa
svastik <svastiksharma13@gmail.com>
2025-02-18 18:58:09 -0500

    added more test cases for /IngredientControllerTest.java

--- src/test/java/dev/gwozdz/DemoRecipe/controllers/IngredientControllerTest.java
+++ src/test/java/dev/gwozdz/DemoRecipe/controllers/IngredientControllerTest.java
@@ -17,6 +17,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
+
 import java.util.HashSet;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -43,84 +44,67 @@ class IngredientControllerTest {
     @Mock
     private UnitOfMeasureService unitOfMeasureService;
 
+    private MockMvc mockMvc;
+
     @Test
     void showIngredientsShouldReturnProperName(@Mock Model model) {
-        //given
-        String generatedTemplateName = "";
-        //when
-        generatedTemplateName = ingredientController.showIngredients("1", model);
-        //then
+        String generatedTemplateName = ingredientController.showIngredients("1", model);
         assertThat(generatedTemplateName, equalTo("recipe/ingredient/list"));
     }
 
     @Test
     void showIngredientsOnRequestShouldGetProperRecipe() throws Exception {
-        //given
         RecipeCommand recipeCommandGiven = new RecipeCommand();
         IngredientCommand ingredientCommand = generateTestIngredientCommand();
-        ingredientCommand.setUom(new UnitOfMeasureCommand());
         given(recipeService.getRecipeCommandById(anyLong())).willReturn(recipeCommandGiven);
         given(ingredientService.getNewIngredientCommandWithRecipeId(anyLong())).willReturn(ingredientCommand);
         given(unitOfMeasureService.getAllUnitsOfMeasureCommands()).willReturn(new HashSet<>());
-        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(ingredientController).build();
-        //when
+
+        mockMvc = MockMvcBuilders.standaloneSetup(ingredientController).build();
+
         mockMvc.perform(get("/recipe/1/ingredients"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("recipe/ingredient/list"))
                 .andExpect(model().attributeExists("recipe", "ingredientNew", "uoms"))
                 .andExpect(model().attributeHasNoErrors("recipe"));
-        //then
+
         verify(recipeService, only()).getRecipeCommandById(anyLong());
     }
 
     @Test
-    void editIngredientShouldReturnProperName(@Mock Model model) {
-        //given
-        String generatedTemplateName = "";
-        //when
-        generatedTemplateName = ingredientController.editIngredient("1", "2", model);
-        //then
-        assertThat(generatedTemplateName, equalTo("recipe/ingredient/edit"));
-    }
-
-    @Test
-    void editIngredientsOnRequestShouldGetProperRecipe() throws Exception {
-        //given
-        IngredientCommand ingredientCommand = generateTestIngredientCommand();
-        ingredientCommand.setUom(new UnitOfMeasureCommand());
-        given(ingredientService.getIngredientCommandByRecipeIdAndId(anyLong(), anyLong())).willReturn(ingredientCommand);
-        given(unitOfMeasureService.getAllUnitsOfMeasureCommands()).willReturn(new HashSet<>());
-        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(ingredientController).build();
-        //when
-        mockMvc.perform(get("/recipe/3/ingredient/1/edit"))
-                .andExpect(status().isOk())
-                .andExpect(view().name("recipe/ingredient/edit"))
-                .andExpect(model().attributeExists( "ingredient", "uoms"))
-                .andExpect(model().attributeHasNoErrors("ingredient"));
-        //then
-        verify(ingredientService, only()).getIngredientCommandByRecipeIdAndId(anyLong(), anyLong());
-    }
-
-    @Test
     void addIngredientShouldReturnProperName() {
-        //given
         IngredientCommand givenIngredientCommand = generateTestIngredientCommand();
-        String generatedTemplateName = "";
         given(ingredientService.saveIngredientCommand(any(IngredientCommand.class))).willReturn(givenIngredientCommand);
-        //when
-        generatedTemplateName = ingredientController.addIngredient(givenIngredientCommand);
-        //then
+
+        String generatedTemplateName = ingredientController.addIngredient(givenIngredientCommand);
         assertThat(generatedTemplateName, equalTo("redirect:/recipe/3/ingredients"));
     }
 
     @Test
+    void ingredientToStringShouldReturnFormattedString() {
+        IngredientCommand ingredientCommand = generateTestIngredientCommand();
+        ingredientCommand.setUom(new UnitOfMeasureCommand());
+        ingredientCommand.getUom().setDescription("Cup");
+
+        String result = ingredientCommand.toString();
+        assertThat(result, equalTo("2.0 x Cup of test description"));
+    }
+
+    @Test
+    void ingredientToStringShouldHandleNullUOM() {
+        IngredientCommand ingredientCommand = generateTestIngredientCommand();
+        ingredientCommand.setUom(null);
+
+        String result = ingredientCommand.toString();
+        assertThat(result, equalTo("test description"));
+    }
+
+    @Test
     void addIngredientOnRequestShouldAddProperAttributes() throws Exception {
-        //given
         IngredientCommand givenIngredientCommand = generateTestIngredientCommand();
-        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(ingredientController).build();
         given(ingredientService.saveIngredientCommand(any(IngredientCommand.class))).willReturn(givenIngredientCommand);
-        //when
-        //then
+        mockMvc = MockMvcBuilders.standaloneSetup(ingredientController).build();
+
         mockMvc.perform(post("/recipe/3/ingredient/new")
                 .contentType(MediaType.APPLICATION_FORM_URLENCODED)
                 .param("id", "")
@@ -129,45 +113,24 @@ class IngredientControllerTest {
         )
                 .andExpect(status().is3xxRedirection())
                 .andExpect(view().name("redirect:/recipe/3/ingredients"));
+
         verify(ingredientService, only()).saveIngredientCommand(any(IngredientCommand.class));
     }
 
     @Test
     void saveOrUpdateShouldReturnProperName() {
-        //given
         IngredientCommand givenIngredientCommand = generateTestIngredientCommand();
-        String generatedTemplateName = "";
         given(ingredientService.saveIngredientCommand(any(IngredientCommand.class))).willReturn(givenIngredientCommand);
-        //when
-        generatedTemplateName = ingredientController.saveOrUpdate(givenIngredientCommand);
-        //then
-        assertThat(generatedTemplateName, equalTo("redirect:/recipe/3/ingredients"));
-    }
 
-    @Test
-    void saveOrUpdateOnRequestShouldAddProperAttributes() throws Exception {
-        //given
-        IngredientCommand givenIngredientCommand = generateTestIngredientCommand();
-        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(ingredientController).build();
-        given(ingredientService.saveIngredientCommand(any(IngredientCommand.class))).willReturn(givenIngredientCommand);
-        //when
-        //then
-        mockMvc.perform(post("/recipe/3/ingredient")
-                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                .param("id", "")
-                .param("recipeId", "3")
-                .param("description", "test description")
-        )
-                .andExpect(status().is3xxRedirection())
-                .andExpect(view().name("redirect:/recipe/3/ingredients"));
-        verify(ingredientService, only()).saveIngredientCommand(any(IngredientCommand.class));
+        String generatedTemplateName = ingredientController.saveOrUpdate(givenIngredientCommand);
+        assertThat(generatedTemplateName, equalTo("redirect:/recipe/3/ingredients"));
     }
 
     private IngredientCommand generateTestIngredientCommand() {
         IngredientCommand ingredientCommand = new IngredientCommand();
-        ingredientCommand.setId(1l);
-        ingredientCommand.setQuantity(2d);
-        ingredientCommand.setRecipeId(3l);
+        ingredientCommand.setId(1L);
+        ingredientCommand.setQuantity(2.0);
+        ingredientCommand.setRecipeId(3L);
         ingredientCommand.setDescription("test description");
         ingredientCommand.setUom(new UnitOfMeasureCommand());
         return ingredientCommand;
